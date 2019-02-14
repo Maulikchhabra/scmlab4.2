b1f39ca7fc722b7e1352a7032e433692ef7d2ecb
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2019-02-14 11:09:17 +0530

    Added Concept of IF ELSE

--- p1.cpp
+++ p1.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
+	cout<<"Enter 1 or 0"<<endl;
+	cin>>a;
+	if(a==1)
 	cout<<"It is Raining Outside"<<endl;
+	else if(a==0)
+	cout<<"It is Not Raining Outside"<<endl;
+	else
+	cout<<"Wrong Input"<<endl;
 }
 
